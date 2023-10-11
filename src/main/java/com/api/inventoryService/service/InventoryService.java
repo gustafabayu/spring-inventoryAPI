9699142c700f8f769c81fd2fd18714dbcad3bd72
Gustafa Bayu <faafa13@gmail.com>
2023-10-11 15:36:18 +0700

    implement circuit breaker

--- src/main/java/com/api/inventoryService/service/InventoryService.java
+++ src/main/java/com/api/inventoryService/service/InventoryService.java
@@ -9,14 +9,21 @@ import com.api.inventoryService.dto.InventoryResponse;
 import com.api.inventoryService.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("wait started");
+        Thread.sleep(3000);
+        log.info("wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                         .map(inventory -> InventoryResponse.builder()
                                 .skuCode(inventory.getSkuCode())
